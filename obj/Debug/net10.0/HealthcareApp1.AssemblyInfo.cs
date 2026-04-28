ca491a0ca00a09f9cdf82590fbe57e1669b359fa
Lavanya-rameshbabu <lashyashree@gmail.com>
2026-04-28 19:59:25 +0800

    Added Screenshots

--- obj/Debug/net10.0/HealthcareApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/HealthcareApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fcc77ef7be983d40de06dc82fbb7a1c8e638d8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
